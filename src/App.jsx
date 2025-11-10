521b4f8d6eb7f2039c9806344d358ee2268bee13
Gustavo Fernandes de Almeida <gustty75rj@hotmail.com>
2025-11-10 20:39:43 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import './App.css'
 import Perfil from './perfil'
+import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
+import NavHeader from './components/aula7/Header'
+import Home from './components/aula7/Home'
+import Product from './components/aula7/Product'
+import Contato from './components/aula7/FaleConosco';
 
-function App() {
-  
-
+const App = () => {
   return (
-<>
-    <Perfil />
-    {'tesdadste'}
-</>
-      
-  )
+    <>
+    <NavHeader />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/produto/:id" element={<Product />} />
+        <Route path="/contato" element={<Contato />} />
+      </Routes>
+    </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
