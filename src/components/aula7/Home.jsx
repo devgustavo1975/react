e8fc9a039e47da8de04f88cf940a4c1ff5925863
Gustavo Fernandes de Almeida <gustty75rj@hotmail.com>
2025-11-17 19:07:42 -0300

    Add files via upload

--- src/components/aula7/Home.jsx
+++ src/components/aula7/Home.jsx
@@ -1,32 +1,38 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import Card from '../components/Card'; // ajuste o caminho conforme seu projeto
 
 const Home = () => {
   const [data, setData] = useState(null);
-    useEffect(() => {
+
+  useEffect(() => {
     axios.get('https://node-vercel-app-rho.vercel.app/api/funcionarios')
       .then((response) => setData(response.data))
       .catch((error) => console.error('Error fetching data:', error));
   }, []);
 
   return (
-    <div className={'home'}>
-      <Box sx={{ flexGrow: 1 }} >
-        <Grid container spacing={2} >
+    <div className="home">
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
           {data?.map((funcionario, index) => (
-            <Grid size={5} >
-              <div key={index}>{funcionario.nome}</div>
+            <Grid item xs={12} sm={6} md={4} key={index}>
+              <Card
+                nome={funcionario.nome}
+                dt_nascimento={funcionario.dt_nascimento}
+                cargo={funcionario.cargo}
+              />
             </Grid>
           ))}
         </Grid>
       </Box>
     </div>
-  )
+  );
 }
 
 export default Home;
+
 
           
